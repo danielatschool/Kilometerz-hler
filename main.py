9ffaafc170532ff485de7ab7287576583d4aef86
danielatschool <daniel.rosemann@lgs-hu.eu>
2025-09-26 11:49:36 +0200

    Improvements

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ km_stand_vorher = float(input("Geben Sie den Kilometerstand beim vorherigen Tank
 km_stand_jetzt = float(input("Geben Sie den Kilometerstand beim jetzigen Tanken ein: "))
 
 tankmenge_liter = float(input("Geben Sie die getankte Menge in Litern ein: "))  
+verbrauch = (tankmenge_liter / (km_stand_jetzt - km_stand_vorher)) * 100
 
-verbrauch = (tankmenge_liter / (km_stand_jetzt - km_stand_vorher)) * 100  
-  
 print(f"Der durchschnittliche Verbrauch beträgt {verbrauch:.2f} Liter pro 100 Kilometer.")
