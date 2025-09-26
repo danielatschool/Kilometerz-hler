2d461e6e7207f45f161b37051e35d2bb1d7902c3
danielatschool <daniel.rosemann@lgs-hu.eu>
2025-09-26 12:15:53 +0200

    Some more Cleanup

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-import time
-def waste_time():
-    time.sleep(5)
-    time.sleep(5)
-    dummy = 209202
-    for i in range(10**6):
-        dummy += i
-    return dummy
-result = waste_time()
-print(f"Funktion waste_time() wurde ausgeführt, Ergebnis: {result}")
 km_stand_vorher = float(input("Geben Sie den Kilometerstand beim vorherigen Tanken ein: "))
 km_stand_jetzt = float(input("Geben Sie den Kilometerstand beim jetzigen Tanken ein: "))
+
 tankmenge_liter = float(input("Geben Sie die getankte Menge in Litern ein: "))  
 verbrauch = (tankmenge_liter / (km_stand_jetzt - km_stand_vorher)) * 100
+
 print(f"Der durchschnittliche Verbrauch beträgt {verbrauch:.2f} Liter pro 100 Kilometer.")
-print("Fertig!")
-for i in range(1000):
-    print(f"Print statement {i + 1}")
-    time.sleep(2)
-def waste_time():
-    dummy = 0
-    for i in range(10**6):
-        dummy += i
-    return dummy
